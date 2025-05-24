ab808c3ad7e53234850533838592534e1a6d63de
Vardhan Hegde <hegdevardhan@gmail.com>
2025-05-24 13:28:41 +0530

    Hot Fix

--- backend/service.py
+++ backend/service.py
@@ -1,33 +1,26 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
 import random
 import math
 
+from questionDB import questions
+
 app = FastAPI()
 
-a = "question"
-b = "answer"
-c=a+"1"
-d=b+"1"
-questions = [[a,b],[c,d]]
+total_questions = len(questions)
 
-@app.get("/")
+@app.get(
+    "/questions",
+    summary="Gets random Question from database"
+)
 def read_root():
-    length = len(questions)
-    loc = math.floor(random.randint(0, length-1))
+    loc = math.floor(random.randint(0, total_questions-1))
     return  questions[loc][0], loc
 
-@app.get("/checkanswer/{question_no}")
+@app.post(
+    "/checkanswer/{question_no}",
+    summary="Find answer for the Question No. provided"
+)
 def chec_ans(question_no:int):
+    if question_no >= total_questions:
+        raise HTTPException(status_code=404, detail="The question no. is out of range")
     return {f"answer is: {questions[question_no-1][1]}"}
-
-@app.post('/')
-def show():
-    pass
-
-@app.delete('/')
-def show():
-    pass
-
-@app.put('/')
-def show():
-    pass
